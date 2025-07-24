87f9d54de0e40d30ebc1a0db980bab323b09435d
Chiru2123 <chyrus2123@gmail.com>
2025-07-24 16:20:54 +0530

    Update SingleResponsibilityPrinciple.java

--- SingleResponsibilityPrinciple.java
+++ SingleResponsibilityPrinciple.java
@@ -1,21 +1,10 @@
-class Invoice {
-    void calculateTotal() {
-        System.out.println("Calculating total...");
+class SingleResponsibilityPrinciple {
+    public void saveOrder(String orderId) {
+        System.out.println("Order " + orderId + " saved to database.");
     }
-}
 
-class InvoicePrinter {
-    void printInvoice() {
-        System.out.println("Printing invoice...");
-    }
-}
-
-public class SingleResponsibilityPrinciple {
     public static void main(String[] args) {
-        Invoice invoice = new Invoice();
-        invoice.calculateTotal();
-
-        InvoicePrinter printer = new InvoicePrinter();
-        printer.printInvoice();
+        SingleResponsibilityPrinciple order = new SingleResponsibilityPrinciple();
+        order.saveOrder("ORD123");
     }
 }
